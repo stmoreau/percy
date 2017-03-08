f3e378a65c38784d810a80ac4fda59fd2b53be29
Stephane Moreau <st.moreau@outlook.com.gr>
2017-03-08 15:39:56 +0000

    storybook and enzyme added

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme'; // test components in isolation from the child components they render
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it('App renders without crashing', () => {
+  shallow(<App />);
 });
