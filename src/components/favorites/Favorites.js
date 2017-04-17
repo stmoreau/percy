3bdbcce585d291d9498213e92c378441eb1ebcea
Stephane Moreau <st.moreau@outlook.com.gr>
2017-04-17 11:33:00 +0100

    basic structure voicemails & favorites

--- src/components/favorites/Favorites.js
+++ src/components/favorites/Favorites.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Menu from './../menu/Menu';
 import TopMenu from './../landing-page/TopMenu.js';
+import FontAwesome from 'react-fontawesome';
 
 class Favorites extends Component {
 
@@ -11,7 +12,58 @@ class Favorites extends Component {
         <div className="body-container">
           <Menu />
           <div className="body-content">
-            this is my content
+            <h4>Your Favorites Voicemails</h4>
+            <table>
+              <tbody>
+                <tr>
+                  <td>
+                    From
+                  </td>
+                  <td>
+                    Play
+                  </td>
+                  <td>
+                    Text
+                  </td>
+                  <td>
+                    Downloads
+                  </td>
+                  <td>
+                    Favorites
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    +447904801802
+                  </td>
+                  <td>
+                    <FontAwesome
+                      className='play'
+                      name='play'
+                      size='2x'
+                      />
+                  </td>
+                  <td>
+                    Dinner is ready! Where are you?
+                  </td>
+                  <td>
+                    <FontAwesome
+                      className='download'
+                      name='download'
+                      size='2x'
+                      />
+                  </td>
+                  <td>
+                    <FontAwesome
+                      className='heart'
+                      name='heart'
+                      size='2x'
+                      style={{ color: 'red' }}
+                      />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
--- src/components/voicemails/Voicemails.js
+++ src/components/voicemails/Voicemails.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Menu from './../menu/Menu';
 import TopMenu from './../landing-page/TopMenu.js';
+import FontAwesome from 'react-fontawesome';
 
 class Voicemails extends Component {
 
@@ -11,7 +12,87 @@ class Voicemails extends Component {
         <div className="body-container">
           <Menu />
           <div className="body-content">
-            this is my content
+            <h4>Received Voicemails</h4>
+            <table>
+              <tbody>
+                <tr>
+                  <td>
+                    From
+                  </td>
+                  <td>
+                    Play
+                  </td>
+                  <td>
+                    Text
+                  </td>
+                  <td>
+                    Downloads
+                  </td>
+                  <td>
+                    Favorites
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    +447904801802
+                  </td>
+                  <td>
+                    <FontAwesome
+                      className='play'
+                      name='play'
+                      size='2x'
+                      />
+                  </td>
+                  <td>
+                    Dinner is ready! Where are you?
+                  </td>
+                  <td>
+                    <FontAwesome
+                      className='download'
+                      name='download'
+                      size='2x'
+                      />
+                  </td>
+                  <td>
+                    <FontAwesome
+                      className='heart'
+                      name='heart'
+                      size='2x'
+                      style={{ color: 'red' }}
+                      />
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    +447904801802
+                  </td>
+                  <td>
+                    <FontAwesome
+                      className='play'
+                      name='play'
+                      size='2x'
+                      />
+                  </td>
+                  <td>
+                    Hey ring me when you are available please. See you!
+                  </td>
+                  <td>
+                    <FontAwesome
+                      className='download'
+                      name='download'
+                      size='2x'
+                      />
+                  </td>
+                  <td>
+                    <FontAwesome
+                      className='heart'
+                      name='heart'
+                      size='2x'
+                      />
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
