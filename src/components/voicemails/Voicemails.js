ad20fda304eebad2aaf0d1b43cb7b7ff1e2d503c
Stephane Moreau <st.moreau@outlook.com.gr>
2017-04-18 22:58:52 +0100

    clean up

--- src/components/voicemails/Voicemails.js
+++ src/components/voicemails/Voicemails.js
@@ -8,26 +8,16 @@ class Voicemails extends Component {
       super();
       this.state = {
         play1: 'play',
-        play2: 'play',
-        color1: 'red',
-        color2: 'black'
+        play2: 'play'
       };
   }
 
-  handleClickPlay1() {
+  handleClick1() {
     (this.state.play1 === 'play') ? this.setState({play1: 'pause'}) : this.setState({play1: 'play'})
   }
 
-  handleClickPlay2() {
+  handleClick2() {
     (this.state.play2 === 'play') ? this.setState({play2: 'pause'}) : this.setState({play2: 'play'})
-  }
-
-  handleClickHeart1() {
-    (this.state.color1 === 'red') ? this.setState({color1: 'black'}) : this.setState({color1: 'red'})
-  }
-
-  handleClickHeart2() {
-    (this.state.color2 === 'red') ? this.setState({color2: 'black'}) : this.setState({color2: 'red'})
   }
 
   render() {
@@ -63,7 +53,7 @@ class Voicemails extends Component {
                   </td>
                   <td>
                     <FontAwesome
-                      onClick={this.handleClickPlay1.bind(this)}
+                      onClick={this.handleClick1.bind(this)}
                       className={this.state.play1}
                       name={this.state.play1}
                       size='2x'
@@ -81,11 +71,10 @@ class Voicemails extends Component {
                   </td>
                   <td>
                     <FontAwesome
-                      onClick={this.handleClickHeart1.bind(this)}
                       className='heart'
                       name='heart'
                       size='2x'
-                      style={{ color: this.state.color1 }}
+                      style={{ color: 'red' }}
                       />
                   </td>
                 </tr>
@@ -95,7 +84,7 @@ class Voicemails extends Component {
                   </td>
                   <td>
                     <FontAwesome
-                      onClick={this.handleClickPlay2.bind(this)}
+                      onClick={this.handleClick2.bind(this)}
                       className={this.state.play2}
                       name={this.state.play2}
                       size='2x'
@@ -113,11 +102,10 @@ class Voicemails extends Component {
                   </td>
                   <td>
                     <FontAwesome
-                      onClick={this.handleClickHeart2.bind(this)}
                       className='heart'
                       name='heart'
                       size='2x'
-                      style={{ color: this.state.color2 }}
+                      style={{ color: 'black' }}
                       />
                   </td>
                 </tr>
